8a0823cfd23df00895c49e335c57e7ecd75943d9
Nick Apperley <napperley@gmail.com>
2018-07-05 14:56:01 +1200

    Switched to Docker for deployment.

--- template/kotlin-jvm/src/main/kotlin/org/example/function/main.kt
+++ template/kotlin-jvm/src/main/kotlin/org/example/function/main.kt
@@ -3,7 +3,6 @@ package org.example.function
 import org.example.function.handler.handle
 
 fun main(args: Array<String>) {
-	print("What is your name: ")
 	val resp = handle(readLine())
 	println(resp)
 }
